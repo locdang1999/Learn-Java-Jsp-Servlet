7a984360c58eef5ff33caa4ad5b66f842d8cec2b
Dang Gia Loc <hahagioi169@gmail.com>
2024-06-02 13:03:47 +0700

    Branch dev1 - RequestDispatcher | Calling a Servlet from Servlet

---
+++ Jsp-Servlet-Tutorial/src/main/java/com/add/SqServlet.java
@@ -0,0 +1,20 @@
+package com.add;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class SqServlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		int sum = (int) request.getAttribute("sum");
+		PrintWriter out = response.getWriter();
+
+		out.print("Hello Sq " + sum);
+	}
+}
