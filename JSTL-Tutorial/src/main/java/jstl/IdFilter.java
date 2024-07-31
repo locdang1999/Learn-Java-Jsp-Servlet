61b3111ed848a590214be6123e1f87f47be8cc58
Dang Gia Loc <hahagioi169@gmail.com>
2024-08-01 00:53:13 +0700

    Branch dev1 -  Servlet Filter

---
+++ JSTL-Tutorial/src/main/java/jstl/IdFilter.java
@@ -0,0 +1,67 @@
+package jstl;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Servlet Filter implementation class IdFilter
+ */
+@WebFilter("/addAlien")
+public class IdFilter extends HttpFilter implements Filter {
+
+	/**
+	 * @see HttpFilter#HttpFilter()
+	 */
+	public IdFilter() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		// TODO Auto-generated method stub
+		// place your code here
+		System.out.println("In filter");
+		
+		PrintWriter out = response.getWriter();
+		
+		HttpServletRequest req = (HttpServletRequest) request;
+		int aid = Integer.parseInt(request.getParameter("aid"));
+		System.out.println("In filter" + aid);
+		
+		if (aid > 1) 	
+			// pass the request along the filter chain
+			chain.doFilter(request, response);
+		else
+			out.println("Invalid Input");
+			
+		
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
